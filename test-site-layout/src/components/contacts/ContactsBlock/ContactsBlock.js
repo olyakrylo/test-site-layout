3d043d4d9d0b9331b369cb7c2604f3c8dfc3a69a
olyakrylo <olyakrylo24@gmail.com>
2020-02-21 18:48:18 +0300

    added contact block and form

---
+++ test-site-layout/src/components/contacts/ContactsBlock/ContactsBlock.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import '../../../css/style.css'
+
+export default class ContactsBlock extends React.Component {
+    sendForm = e => {
+        // e.preventDefault()
+        alert(
+            'name: ' + document.getElementById('form-name').value +
+            '\nemail: ' + document.getElementById('form-email').value +
+            '\ntext: ' + document.getElementsByTagName('textarea')[0].value
+        )
+    }
+
+    render() {
+        let infoText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,'
+        return (
+            <div className='contacts'>
+                <div className='contacts__info'>
+                    <h2>Contact Us</h2>
+                    <p>{infoText}</p>
+                </div>
+                <div className='contacts__content'>
+                    <ul className='contacts__items'>
+                        <li>
+                            <div className='item__background'>{' '}</div>
+                            <p>Nobinagar savar, Dhaka<br/>Bangadesh</p>
+                        </li>
+                        <li>
+                            <div className='item__background'></div>
+                            <p>+8801743331996<br/>+8801928737807</p>
+                        </li>
+                        <li>
+                            <div className='item__background'></div>
+                            <p>quickmasud@gmail.com<br/>quickmasud@yahoo.com</p>
+                        </li>
+                    </ul>
+                    <div className='contacts__form'>
+                        <div className='userinfo'>
+                            <input type='text' placeholder='Your NAME' id='form-name'></input>
+                            <input type='text' placeholder='Your Email' id='form-email'></input>
+                        </div>
+                        <textarea placeholder='Write Message'></textarea>
+                        <button onClick={this.sendForm}>SEND</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
