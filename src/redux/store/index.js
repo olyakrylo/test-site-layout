67f49358f15628f85de6af638c7230b472a5826f
Olga <olyakrylo24@gmail.com>
2020-03-02 22:23:41 +0300

    deleted useful files

--- src/redux/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import reducer from "../reducers";
-
-const initialState = {
-    showingComponent: 'page'
-};
-export const store = createStore(reducer, initialState);
